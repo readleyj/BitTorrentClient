1036d5779f1eaefb3e0b78aa91c40745902e2155
readleyj <readleyj@gmail.com>
2020-05-06 22:08:24 +0400

    Added client and protocol info

--- config.py
+++ config.py
@@ -1,2 +1,14 @@
+# Client Info
+PEER_NAME = 'ET'
+VERSION_NUM = '0001'
+PORT_NUM = 6889
+
+# Connection Parameters
 PIECE_LENGTH = 20
 COMPACT_PEER_SIZE = 6
+
+# Protocol Info
+pstrlen = 19
+pstr = b'BitTorrent protocol'
+reserved_bytes = b'\0' * 8
+HANDSHAKE_LENGTH = 68
